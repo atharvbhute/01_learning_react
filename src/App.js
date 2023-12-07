5eafbde37bb128c4de1723298d4e1e7f8b07d8c2
atharvbhute <atharv.bhute18@gmail.com>
2023-12-08 01:33:22 +0530

    Learning React

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
 function App() {
+
+  const [count, setCount] = useState(0)
+
+  function minus(){
+    setCount(count-1)
+  }
+  function plus(){
+    setTimeout(()=>{console.log("plus1");setCount(count => count +1 )},1000)
+    setTimeout(()=>{console.log("plus1");setCount(count => count +1 )},2000)
+    setTimeout(()=>{console.log("plus1");setCount(count => count +1 )},3000)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <button onClick={minus}> - </button>
+    <h1> {count} </h1>
+    <button onClick={plus}> + </button>
+    </>
+    
   );
 }
 
